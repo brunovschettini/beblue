e615d4a04dbe2c6faaa66d12b395572c4eb6ceae
Bruno Schettini <brunovschettini@outlook.com>
2019-02-01 13:09:58 -0200

    - Repositório do Spotify

--- src/main/java/br/com/beblue/ws/DisksWS.java
+++ src/main/java/br/com/beblue/ws/DisksWS.java
@@ -2,6 +2,7 @@ package br.com.beblue.ws;
 
 import br.com.beblue.utils.NotifyResponse;
 import com.google.gson.Gson;
+import com.wrapper.spotify.SpotifyApi;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -11,6 +12,7 @@ import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+// https://github.com/thelinmichael/spotify-web-api-java
 @Path("/disks")
 public class DisksWS {
 
@@ -24,6 +26,17 @@ public class DisksWS {
         Gson gson = new Gson();
         NotifyResponse notifyResponse = new NotifyResponse();
         notifyResponse.setObject("OK");
+
+        SpotifyApi spotifyApi = new SpotifyApi.Builder()
+                .setClientId("0a487071817e4899813ed05116175fa3")
+                .setClientSecret("fb100b74e7bd43cca5dc4d62872ce62b")
+                // .setRedirectUri("<your_redirect_uri>")
+                .build();
+        
+        String token = spotifyApi.getAccessToken();
+        
+        
+
         return Response.status(200).entity(gson.toJson(notifyResponse)).build();
     }
 
